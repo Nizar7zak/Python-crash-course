bdebec7f9006ca72555d8303aaa332ef567918ca
nizar7zak <nizar7zakout@gmail.com>
2022-10-24 14:57:16 +0300

    for loops

---
+++ 18-for-loops.py
@@ -0,0 +1,16 @@
+for item in ['AWS', 'EC2', 'S3', 'VPC']:
+    print(item)
+
+
+for num in [1, 2, 3, 4, 5, 6, 7]:
+    print(num)
+
+
+for ip in range(10):
+    print(ip)
+
+for ip in range(5, 10):
+    print(ip)
+
+for ip in range(5, 10, 2):
+    print(ip)
