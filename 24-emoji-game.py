c6297d3172ed4ba765ee6c0814613a08ab19f181
nizar7zak <nizar7zakout@gmail.com>
2022-10-25 11:18:05 +0300

    function

--- 24-emoji-game.py
+++ 24-emoji-game.py
@@ -2,13 +2,13 @@ message = input('>')
 
 words = message.split(' ')
 
-emojies = {
+emojis = {
     "happy": ":)",
     "sad": ":("
 }
 
 output = ""
 for word in words:
-    output += emojies.get(word, word) + " "
+    output += emojis.get(word, word) + " "
 
 print(output)
---
+++ 25-functions.py
@@ -0,0 +1,8 @@
+def greet_user():
+    print('Hi there!')
+    print('Welcome abroad')
+
+
+print("START")
+greet_user()
+print("FINISH")
