efd0124dce20bc234741fcadee7fa818f7423679
nizar7zak <nizar7zakout@gmail.com>
2022-10-25 11:26:20 +0300

    parameter

---
+++ 26-parameters.py
@@ -0,0 +1,11 @@
+def greet_user(name):
+    print(f'Hi {name}!')
+    print('Welcome abroad')
+
+
+print("START")
+greet_user("Nizar")
+# greet_user("Momen")
+# greet_user()
+print("FINISH")
+#  let's try 2 parameter as first name and last name
