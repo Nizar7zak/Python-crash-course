60001818a6ac5d022a5b5bae828829c1806f26a1
nizar7zak <nizar7zakout@gmail.com>
2022-10-25 14:07:46 +0300

    classes

---
+++ OOP/classes.py
@@ -0,0 +1,22 @@
+# Numbers
+# Strings
+# Boolean
+
+# Lists
+# Dictionaries
+
+# classes and objects
+
+class Point:
+    def move(self):
+        print("move")
+
+    def draw(self):
+        print("draw")
+
+
+point1 = Point()
+point1.draw()
+
+point1.x = 10
+print(point1.x)
