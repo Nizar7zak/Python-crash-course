f1929307a582abdd3d446836e31f904951f3a215
nizar7zak <nizar7zakout@gmail.com>
2022-10-25 12:16:07 +0300

    exceptions

---
+++ 29-exception.py
@@ -0,0 +1,16 @@
+age = int(input("Age: "))
+print(age)
+
+
+# try:
+#     age = int(input("Age: "))
+#     income = 4000
+#     risk = income / age
+#     print(age)
+# except ZeroDivisionError:
+#   print('your age must be greater than zero :D ')
+# except ValueError:
+#     print('insert a valid number as an age')
+
+
+#  what is the benefit of using try except ?
