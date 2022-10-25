75f622ee2cf465990b95ac5e377a9ceca4020809
nizar7zak <nizar7zakout@gmail.com>
2022-10-25 10:28:25 +0300

    translator game

---
+++ 24-translator.py
@@ -0,0 +1,18 @@
+phone = input("phone: ")
+digits_mapping = {
+    "1": "One",
+    "2": "Two",
+    "3": "Three",
+    "4": "Four",
+    "5": "Five",
+    "6": "Six",
+    "7": "Seven",
+    "8": "Eight",
+    "9": "Nine",
+    "0": "Zero"
+}
+
+output = ""
+for ch in phone:
+    output += digits_mapping.get(ch, "!") + " "
+print(output)
