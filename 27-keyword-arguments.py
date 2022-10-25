fe97cf4de117c7617985b56f0b58e7223c404e68
nizar7zak <nizar7zakout@gmail.com>
2022-10-25 11:42:10 +0300

    keywords-argument

---
+++ 27-keyword-arguments.py
@@ -0,0 +1,11 @@
+def greet_user(first_name, last_name):
+    print(f'Hi {first_name} {last_name}!')
+    print('Welcome abroad')
+
+
+# greet_user("Nizar", "Zakout")
+# greet_user(first_name="Nizar", last_name="Zakout")
+# greet_user(last_name="Nizar", first_name="Zakout")
+# greet_user(last_name="Nizar", "Zakout")
+# greet_user("Nizar", last_name="Zakout")
+# greet_user("Nizar", first_name="Zakout")
