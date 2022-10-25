88ae0d4d1e6efbe7a24368140fcd9c0c5e3500c1
nizar7zak <nizar7zakout@gmail.com>
2022-10-25 14:42:17 +0300

    inheritance

---
+++ OOP/03-inheritance.py
@@ -0,0 +1,22 @@
+
+class Female:
+    def description(self):
+        print("AWS - Student")
+
+
+class Male:
+    def description(self):
+        print("AWS - Student")
+
+
+# class Candidate:
+#     def description(self):
+#         print("AWS - Student")
+
+
+# class Female(Candidate):
+#     pass
+
+
+# class Male(Candidate):
+#     pass
